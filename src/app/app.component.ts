5ee679552dc2b4bc5becb586090da446493be37d
mert.alptekin <mert.alptekin@neomina.com>
2022-11-25 16:41:39 +0300

    1-Ngb Boostrap Kullanımı 2- MomentJs ve Loadash Kütüphenesi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import * as _ from 'lodash';  
+import * as moment from 'moment';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    console.log('moment', moment().add(1,'years'));
+    console.log('moment-locale', moment().locale());
+    console.log('moment', moment().format('DD/MMMM/yyyy HH:mm'));
+  }
   title = 'AngularUIApp';
+  message= 'Alert Message';
+
+  data = [{name:'ali'},{name:'ahmet'}]
+
+  @ViewChild('alert') alert!: NgbAlert;
+
+
+
+
+  onClose(){
+    this.alert.close();
+
+
+  
+
+  }
 }
